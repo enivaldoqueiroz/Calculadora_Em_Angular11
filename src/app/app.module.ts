3af0045d39b091f65f2fce03e05fd84b1dd2b150
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-01-31 09:26:09 -0300

    Projeto Calculadora Angular 11

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { from } from 'rxjs';
+
+import { AppComponent } from './app.component';
+import {CalculadoraModule} from './calculadora/calculadora.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    CalculadoraModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
